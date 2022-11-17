dfabf7ed0e34bf9a4256aa906aec054d44700976
jnanaswaroop <118235880+jnanaswaroop@users.noreply.github.com>
2022-11-17 17:06:41 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -3,7 +3,7 @@ echo "Enter the number you want to check for even or odd"
 read n
 if [ `expr $n % 2` -eq 0 ]
 then
-	echo "Entered number is an Even number Jnana swaroop k r"
+	echo "Entered number is an Even number Jnana swaroop k r Pratap"
 else
 	echo "Entered number is an Odd number"
 fi
